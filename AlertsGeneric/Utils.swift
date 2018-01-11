7ef493a051fd586a71f286d2a6502c04bee30291
Miguel Mexicano Herrera <miguel.mexicano@gonet.us>
2018-01-11 13:00:26 -0600

    add button for show alerts

--- AlertsGeneric/Utils.swift
+++ AlertsGeneric/Utils.swift
@@ -10,19 +10,26 @@ import UIKit
 
 class Utils: NSObject {
     
-    func alerta(context: UIViewController, title: String, mensaje: String, actions: [UIAlertAction] ){
-        print(actions)
-        
+    func alertaWithAction(context: UIViewController, title: String, mensaje: String, actions: UIAlertAction...){
         let alert = UIAlertController(title: title, message: mensaje, preferredStyle: UIAlertControllerStyle.alert)
-        
-        for action in actions{
-            alert.addAction(action)
-        }
-        
-        
+            for action in actions{
+                alert.addAction(action)
+            }
         //alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
         context.present(alert, animated: true, completion: nil)
     }
+    
+    
+    
+    func alerta(context: UIViewController, title: String, mensaje: String,actionText: String){
+        let alert = UIAlertController(title: title, message: mensaje, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: actionText, style: UIAlertActionStyle.default, handler: nil))
+        context.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+    
     
 
 }
--- AlertsGeneric/ViewController.swift
+++ AlertsGeneric/ViewController.swift
@@ -10,42 +10,38 @@ import UIKit
 
 
 class ViewController: UIViewController {
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        
-    }
-    
-    
-    
-    override func viewDidAppear(_ animated: Bool) {
-        let acccion1 = UIAlertAction(title: "opcion1", style: .default) { (resp) in
-            print("accion1");
-        }
-        let acccion2 = UIAlertAction(title: "opcion2", style: .default) { (resp) in
-            print("accion2");
-        }
-        let acccion3 = UIAlertAction(title: "opcion3", style: .default) { (resp) in
-            print("accion1");
-        }
-    
-        var arrayaccion = [UIAlertAction]()
-    
-        arrayaccion.append(acccion1)
-        arrayaccion.append(acccion2)
-        arrayaccion.append(acccion3)
-        
-        Utils().alerta(context: self, title: "prueba", mensaje: "solo pruebo", actions: arrayaccion)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    @IBAction func showAlert(_ sender: Any) {
+        let acccion1 = UIAlertAction(title: "opcion1", style: .default) { (resp) in
+            Utils().alerta(context: self, title: "opcion", mensaje: "opcion1", actionText: "Cerrar")
+        }
+        let acccion2 = UIAlertAction(title: "opcion2", style: .default) { (resp) in
+            Utils().alerta(context: self, title: "opcion", mensaje: "opcion2", actionText: "Cerrar")
+        }
+        let acccion3 = UIAlertAction(title: "opcion3", style: .default) { (resp) in
+            Utils().alerta(context: self, title: "opcion", mensaje: "opcion3", actionText: "Cerrar")
+        }
+        
+        /*var arrayaccion = [UIAlertAction]()
+         
+         arrayaccion.append(acccion1)
+         arrayaccion.append(acccion2)
+         arrayaccion.append(acccion3)*/
+        
+        Utils().alertaWithAction(context: self, title: "prueba", mensaje: "solo pruebo", actions: acccion1,acccion2,acccion3)
+        
+    }
+    
 
 }
 
